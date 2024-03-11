6c8ef573a388dfd5d19af1b4813e6777d5b080ff
juan3333333 <jsantana@elpuig.xeill.net>
2024-03-11 13:32:32 +0100

    8

--- 2D Plataforma Credito de sintesis/scenes/main_character.gd
+++ 2D Plataforma Credito de sintesis/scenes/main_character.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var sprite_2d = $Sprite2D
 
 # Get the gravity / RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,8 +11,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# gravity.
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping"
 
 	# jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -25,3 +32,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
