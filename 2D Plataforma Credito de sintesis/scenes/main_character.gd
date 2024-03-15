e4438e168ec155fe842d7cc0c17217fe572b27d5
juan3333333 <jsantana@elpuig.xeill.net>
2024-03-15 12:41:38 +0100

    10

--- 2D Plataforma Credito de sintesis/scenes/main_character.gd
+++ 2D Plataforma Credito de sintesis/scenes/main_character.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
+#Tiempo de salto
+var coyote_time = 0.1
+var can_jump = false
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -800.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -700.0
 @onready var sprite_2d = $Sprite2D
+
+
+
 
 # Get the gravity / RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,22 +22,34 @@ func _physics_process(delta):
 	else:
 		sprite_2d.animation = "default"
 
-	if not is_on_floor():
+	if  not is_on_floor():
 		velocity.y += gravity * delta
 		sprite_2d.animation = "jumping"
+	
+	if is_on_floor() and can_jump == false:
+		can_jump = true
+	elif can_jump == true and $coyoteTimer.is_stopped():  
+		$coyoteTimer.start(coyote_time)
 
 	# jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	
+	if Input.is_action_just_pressed("jump") and can_jump == true:
 		velocity.y = JUMP_VELOCITY
-
+		can_jump = false
+		
 	# movement/deceleration.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 15)
+		velocity.x = move_toward(velocity.x, 0, 30)
 
 	move_and_slide()
 	
 	var isLeft = velocity.x < 0
 	sprite_2d.flip_h = isLeft
+	
+
+func _on_coyote_timer_timeout():
+	can_jump = false
+	pass # Replace with function body.
